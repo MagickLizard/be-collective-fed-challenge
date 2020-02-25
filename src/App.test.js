1fab0f6aa8ffd54c01bee018e6a9770e70cdbb8f
magicklizard <lizzieking521@gmail.com>
2020-02-25 23:27:24 +1100

    Added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import App from './App';
+import FileBrowser from './components/file-browser'
 import { shallow, mount, render } from 'enzyme';
 
-test('renders File Browser text', () => {
-  const { getByText } = render(<App />);
-});
 
+describe('<App/>', () => {
+  it('Values on load', async () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state('totalSize')).toEqual(0);
+    expect(wrapper.find('.container')).toBeDefined();
+    expect(wrapper.find('.wrapper')).toBeDefined();
+    expect(wrapper.state('loading')).toBe(true)
+  });
+  it('On componentDidMount data should exist', async () => {
+    const wrapper = shallow(<App />);
+    await wrapper.instance().componentDidMount()
+    expect(wrapper.find('.wrapper')).toBeDefined();
+    expect(wrapper.state('loading')).toBe(false);
+    expect(wrapper.state('totalSize')).toBeGreaterThan(0);
+    expect(wrapper.state('data').length).toBeGreaterThan(0);
+  });
+
+});
