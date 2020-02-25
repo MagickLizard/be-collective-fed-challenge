9a391af82ea0eee1663b3755e60ddbd9ebb5b23f
magicklizard <lizzieking521@gmail.com>
2020-02-25 23:40:57 +1100

    Added mocking api and data responses

--- src/App.test.js
+++ src/App.test.js
@@ -12,6 +12,7 @@ describe('<App/>', () => {
     expect(wrapper.find('.wrapper')).toBeDefined();
     expect(wrapper.state('loading')).toBe(true)
   });
+
   it('On componentDidMount data should exist', async () => {
     const wrapper = shallow(<App />);
     await wrapper.instance().componentDidMount()
@@ -21,4 +22,28 @@ describe('<App/>', () => {
     expect(wrapper.state('data').length).toBeGreaterThan(0);
   });
 
+  it('Should calculateTotals and update state', async () => {
+    const wrapper = shallow(<App />);
+    const fixtureOfApiResponse = [ { type: 'folder',
+    name: '1080p',
+    children: [] },
+  { type: 'file', name: 'borders_orchard_kroon.pdf', size: 76034 },
+  { type: 'folder',
+    name: 'wireless',
+    children: [] } ]
+
+     await wrapper.instance().calculateTotals(fixtureOfApiResponse)
+
+    expect(wrapper.find('.container')).toBeDefined();
+    expect(wrapper.state('data')).toBeDefined();
+    expect(wrapper.state('totalSize')).toBeGreaterThan(0);
+  });
+  it('Should try to calculateTotals() when data is bad', async () => {
+    const wrapper = shallow(<App />);
+     await wrapper.instance().calculateTotals([{type: "file"}])
+console.log('wrapper.instance()>>',wrapper.instance())
+
+    expect(wrapper.find('.container')).toBeDefined();
+    expect(wrapper.state('error')).toBeDefined();
+  });
 });
