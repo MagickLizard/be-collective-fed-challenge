6e1527668c63e7651f19070a52adab4b9c994f6a
magicklizard <lizzieking521@gmail.com>
2020-02-26 16:41:41 +1100

    added file icon tests

---
+++ src/components/file-icons.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import FileIcons from './file-icons'
+import { shallow, mount, render } from 'enzyme';
+import { validResponse, invalidFoldersResponse } from '../__mocks/__axios';
+
+const selectedFolders = {}
+const handleFileClickMock = (selectedfolderId) => {
+    if (selectedFolders[selectedfolderId]) {
+        // remove selected key from files list
+        delete selectedFolders[selectedfolderId];
+    } else {
+        selectedFolders[selectedfolderId] = {}
+    }
+    console.log('selectedFolders', selectedFolders);
+    onChange(selectedFolders)
+}
+
+describe('<FileIcons/>', () => {
+    it('Should contain values on load', async () => {     
+        const folder = validResponse.data.data[0];
+        const wrapper = mount(<FileIcons
+            selected={{}}
+            label={folder.name}
+            type={folder.type}
+            size={folder.size}
+            onChange={() => { handleFileClickMock(`id-${0}`) }}
+        />);
+    
+        expect(wrapper.find('label')).toBeDefined();
+        expect(wrapper.find('size')).toBeDefined();
+        expect(wrapper.find('onChange')).toBeDefined();
+        expect(wrapper.find('files')).toBeDefined();
+        expect(wrapper.find('ul')).toBeDefined();
+        expect(wrapper.find('fa files__icon')).toBeDefined();
+
+    });
+    it('Should have no chevrons down when no folders are selected.', async () => {     
+        const folder = validResponse.data.data[0];
+        const wrapper = shallow(<FileIcons
+            selected={{}}
+            label={folder.name}
+            type={folder.type}
+            size={folder.size}
+            onChange={() => { handleFileClickMock(`id-${0}`) }}
+        />);
+        expect(wrapper.find('fa files__icon')).toBeDefined();
+        expect(wrapper.find('fa files__icon fa-folder')).toBeDefined();
+        expect(wrapper.find('fa files__icon fa-chevron-right')).toBeDefined();
+        expect(wrapper).not.toHaveProperty('fa files__icon fa-chevron-down');
+    });
+    it('Should change chevron to down on selected folder', async () => {     
+
+        const folder = validResponse.data.data[0];
+        const wrapper = shallow(<FileIcons
+            selected={folder}
+            label={folder.name}
+            type={folder.type}
+            size={folder.size}
+            onChange={() => { handleFileClickMock(`id-${0}`) }}
+        />);
+        expect(wrapper).not.toHaveProperty('fa files__icon fa-chevron-down');
+        expect(wrapper.find('fa files__icon')).toBeDefined();
+        expect(wrapper.find('fa files__icon fa-folder')).toBeDefined();
+
+        wrapper.simulate('mouseenter')
+        wrapper.simulate('mouseleave')
+        expect(wrapper.find('fa files__icon fa-chevron-down')).toBeDefined();
+    });
+});
